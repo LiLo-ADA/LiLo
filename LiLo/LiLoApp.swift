129a2b3bdb9143623c52b97a1ddd89576ea1c03a
darren thiorres <darrenthiores@gmail.com>
2024-03-27 16:30:24 +0700

    Initial Commit

---
+++ LiLo/LiLoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiLoApp.swift
+//  LiLo
+//
+//  Created by Darren Thiores on 27/03/24.
+//
+
+import SwiftUI
+
+@main
+struct LiLoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
