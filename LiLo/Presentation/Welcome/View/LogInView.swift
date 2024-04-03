2a33ee21eb33615aa21e2ec0d8a03fbc0205fb16
senakristiawan <senakristiawan1@gmail.com>
2024-04-03 11:02:36 +0700

    Welcome Login

---
+++ LiLo/Presentation/Welcome/View/LogInView.swift
@@ -0,0 +1,18 @@
+//
+//  LogInView.swift
+//  LiLo
+//
+//  Created by Sena Kristiawan on 03/04/24.
+//
+
+import SwiftUI
+
+struct LogInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LogInView()
+}
